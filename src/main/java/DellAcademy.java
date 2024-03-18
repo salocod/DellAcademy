10a543e4bd49741348100327aab6b60c444380e0
salocod <salocod@icloud.com>
2024-03-18 15:55:58 -0300

    polimento

--- src/main/java/DellAcademy.java
+++ src/main/java/DellAcademy.java
@@ -1,9 +1,1 @@
-import Interface.JanelaPrincipalGUI;
-
-public class DellAcademy {
-//FALTA ORDENDAR A LISTA DE VENCEDORES
-//FALTA FAZER A PARTE DE PREMIACAO
-    public static void main(String[] args) {
-        new JanelaPrincipalGUI();
-    }
-}
+public class DellAcademy {public static void main(String[] args) {new Interface.JanelaPrincipalGUI();}}
